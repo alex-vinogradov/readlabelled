34da051b7bccf1847b2b2b01d147645d7c36f486
Alexander Vinogradov <a.g.vinogradov@gmail.com>
2020-07-18 20:56:55 +0300

    init

---
+++ R/read_labelled.R
@@ -0,0 +1,31 @@
+#' Imports data frame from Excel file with variable labels
+#' @param file Excel workbook with two sheets
+#' @param data.sheet Name or number of sheet that contains data
+#' @param vars.sheet Name or number of sheet that contains data dictionary
+#' @examples
+#' # read_labelled("example.xlsx")
+#' # read_labelled("example.xlsx", "Data View", "Variable View")
+read_labelled <- function(file, data.sheet = 1, vars.sheet = 2) {
+  data <- readxl::read_excel(file, sheet = data.sheet)
+  vars <- readxl::read_excel(file, sheet = vars.sheet)
+
+  if (!"variable" %in% names(vars)) stop("There is no 'variable' column in dictionary", call. = FALSE)
+  if (!"label" %in% names(vars)) stop("There is no 'label' column in dictionary", call. = FALSE)
+  if (!"values" %in% names(vars)) stop("There is no 'values' column in dictionary", call. = FALSE)
+
+  varnames <- names(data)
+
+  for (var in vars$variable) {
+    if (var %in% varnames) {
+      varlab <- vars[vars$variable == var, "label", TRUE]
+      if (is.na(varlab)) varlab <- NULL
+      values <- vars[vars$variable == var, "values", TRUE]
+      if (!is.na(values)) values <- unlist(jsonlite::fromJSON(values)) else values <- NULL
+      data[[var]] <- haven::labelled(data[[var]], values, varlab)
+      attr(data[[var]], "format.spss") <- "F8.0"
+    } else {
+      cat("Variable does not exist:", var, "\n")
+    }
+  }
+  return(data)
+}
